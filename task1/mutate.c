7a316c66a0e5e72de0623b6d781fa8461abe21b4
Shane Harding <sharding@tcd.ie>
2015-01-27 22:13:12 +0000

    fixing mutate fn

--- task1/mutate.c
+++ task1/mutate.c
@@ -1,16 +1,16 @@
+#include<stdlib.h>
+
 void showbits(unsigned int x);
 
 int mutate(unsigned int x, double prob){
 
   double r;
   int s;
-//unsigned int t;
+
   s=rand()%32;
   r=drand48();
 
   if(r<prob){
-    //t=1<<s;
-    //showbits(t);
     x^=(1<<s);
   }
   return x;
