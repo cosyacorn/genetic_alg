2a71573a95c72950bc55c43312e166303f39ea37
Shane Harding <sharding@tcd.ie>
2015-01-28 09:52:23 +0000

    adding mutate to task2

--- task2/mutate.c
+++ task2/mutate.c
@@ -4,13 +4,12 @@ int mutate(unsigned int x, double prob){
 
   double r;
   int s;
-//unsigned int t;
+
   r=drand48();
   s=rand()%32;
 
   if(r<prob){
-    //t=1<<s;
-    //showbits(t);
+
     x^=(1<<s);
   }
   return x;
