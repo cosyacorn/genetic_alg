5bc119525752bfce5670bd52ab4ec67944f08850
Shane Harding <sharding@tcd.ie>
2015-01-28 18:18:00 +0000

    changed crossover for PD

--- task2/crossover.c
+++ task2/crossover.c
@@ -1,24 +1,30 @@
 #include<stdlib.h>
 
 int mutate(unsigned int x, double rate);
-int showbits(unsigned int x);
 
-int crossover(unsigned int x, unsigned int y){
+int crossover(unsigned int x, unsigned int y, double cross_rate){
 
   unsigned int z, x1, x2, y1, y2;
   int r;
+  double s;
 
-  r=1+rand()%30;
+  s=drand48();
 
-  x1 = x >> r;
-  x2 = x1 << r;
+  if(s>corss_rate){
+    return x;
+  }
+  else{
 
-  y1 = y << (32-r);
-  y2 = y1 >> (32-r);
+    r=1+rand()%30;
 
-  z = x2|y2;
-  z=mutate(z,0.1);
-  return z;
+    x1 = x >> r;
+    x2 = x1 << r;
+    
+    y1 = y << (32-r);
+    y2 = y1 >> (32-r);
 
-
+    z = x2|y2;
+    z=mutate(z,0.1);
+    return z;
+  }
 }
